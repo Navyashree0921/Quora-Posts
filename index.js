a04fa30d7e2cce9b21e00c888cd75ed9097e85ce
Navyashree J S <navyashree0918email@example.com>
2025-09-22 13:04:23 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+let posts = [
+    {
+        id: uuidv4(),
+        username: "Navyashree",
+        content: "Eat an apple to stay away from doctor!",
+    },
+    {
+        id: uuidv4(),
+        username: "Shibna",
+        content: "Busy in completing CAED Sketch book!",
+    },
+    {
+        id: uuidv4(),
+        username: "Deepak",
+        content: "Busy in buying stuff!",
+    }
+];
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+});
+app.get("/post/new", (req, res) => {
+    res.render("new.ejs");
+});
+app.post("/posts", (req, res) => {
+    let { username, content } = req.body;
+    let id = uuidv4();
+    posts.push({ id, username, content });
+    res.redirect("/posts");
+});
+app.get("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    console.log(id);
+    res.render("show.ejs", { post });
+});
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    res.redirect("/posts");
+});
+app.get("/posts/:id/edit", (req, res) => {
+    let { id } = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", { post });
+});
+app.delete("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    post = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
+});
+app.listen(port, () => {
+    console.log("Listening to port : 8080")
+});
